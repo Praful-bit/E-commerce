ea15fbb69211ef7adc425d6e172e83d2423290ef
Praful Gahlot <prafulgahlot0896@gmail.com>
2024-05-16 23:26:22 +0530

    Auto logout

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { AuthContext } from "../../Context/AuthContext";
 import { NavLink, useLocation } from "react-router-dom";
 import Cart from "../Cart/Cart";
@@ -8,13 +8,30 @@ function Header({ setCartActive }) {
   const { logout, token } = useContext(AuthContext);
   const location = useLocation();
   const showCartButton = location.pathname === "/";
+  const [isLoggedIn, setIsLoggedIn] = useState(!!token);
+ 
+
+  useEffect(() => {
+      
+    if (isLoggedIn) {
+      setTimeout(() => {
+        logout();
+        setIsLoggedIn(false);
+      }, 5 * 60 * 1000);
+    }
+
+  }, [isLoggedIn, logout, ]);
+
   return (
     <nav className="bg-black text-white">
       <div className="container mx-auto flex justify-center">
         <div className="flex items-center space-x-16 pt-2 pb-2 ">
           <button
             className="font-serif cursor-pointer"
-            onClick={() => logout(token)}
+            onClick={() => {
+              logout();
+              setIsLoggedIn(false);
+            }}
           >
             Log out
           </button>
--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
 import { createContext, useState } from "react";
 
 export const AuthContext = createContext({
